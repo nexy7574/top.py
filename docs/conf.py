6b660a45db3087eaab3c8f77a1a3a04127a30d71
Nexus <eek@clicksminuteper.net>
2021-09-15 20:54:43 +0100

    Fix RTD?

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ version = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.duration", "sphinx.ext.intersphinx", "sphinx_rtd_dark_mode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.duration", "sphinx.ext.intersphinx", "sphinx_rtd_theme"]
 
 intersphinx_mapping = {
     "discord": ("https://discordpy.readthedocs.io/en/stable", None),
