784518946bca82bb06df292607a310af40eb122c
EEKIM10 <eek@clicksminuteper.net>
2022-08-26 21:53:54 +0100

    Make model fields optional

--- toppy/models/user.py
+++ toppy/models/user.py
@@ -97,10 +97,10 @@ class SimpleUser(_ReprMixin):
 
     def __init__(self, **kwargs):
         self.id: int = int(kwargs.pop("id"))
-        self.discriminator: str = kwargs.pop("discriminator", "#0000")
-        self.username: str = kwargs.pop("username")
+        self.discriminator: str = kwargs.get("discriminator", "#0000")
+        self.username: str = kwargs.get("username")
         self.avatar: Optional[str] = calculate_avatar_url(
-            self.id, int(self.discriminator[1:]), kwargs.pop("avatar", None)
+            self.id, int(self.discriminator[1:]), kwargs.get("avatar", None)
         )
 
 
@@ -123,11 +123,11 @@ class Socials(_ReprMixin):
     def __init__(self, **kwargs):
         # NOTE:
         # We don't resolve URLs here. We're gonna leave that up to the user.
-        self.youtube = kwargs.pop("youtube", None)
-        self.reddit = kwargs.pop("reddit", None)
-        self.instagram = kwargs.pop("instagram", None)
-        self.github = kwargs.pop("github", None)
-        self.twitter = kwargs.pop("twitter", None)
+        self.youtube = kwargs.get("youtube", None)
+        self.reddit = kwargs.get("reddit", None)
+        self.instagram = kwargs.get("instagram", None)
+        self.github = kwargs.get("github", None)
+        self.twitter = kwargs.get("twitter", None)
 
 
 class User(UserABC, _ReprMixin):
@@ -135,23 +135,27 @@ class User(UserABC, _ReprMixin):
     Model representing a top.gg user's account.
 
     This class conforms to the discord.User ABC.
+
+    .. warning::
+        For some reason, top.gg just randomly omits fields that it shouldn't according to their docs.
+        As such, any or all fields here *may* be None.
     """
 
     def __init__(self, **kwargs):
         self._id: int = int(kwargs.pop("id"))
-        self._username: str = kwargs.pop("username")
-        self._discriminator: str = kwargs.pop("discriminator")
+        self._username: str = kwargs.get("username")
+        self._discriminator: str = kwargs.get("discriminator")
         self._default_avatar: str = default_avatar_url(int(self._discriminator))
-        self._avatar: str = kwargs.pop("avatar", None)
-        self._bio: Optional[str] = kwargs.pop("bio", None)
-        self._banner_url: Optional[str] = kwargs.pop("banner", None)
-        self._socials = kwargs.pop("social", {})
+        self._avatar: str = kwargs.get("avatar", None)
+        self._bio: Optional[str] = kwargs.get("bio", None)
+        self._banner_url: Optional[str] = kwargs.get("banner", None)
+        self._socials = kwargs.get("social", {})
         self._raw_colour = (kwargs.get("color", "0") or "0").lstrip("#")  # can be empty, for some reason.
         self._colour: Colour = Colour(int(self._raw_colour, base=16))
-        self._supporter: bool = kwargs.pop("supporter", False)
-        self._site_mod: bool = kwargs.pop("mod") or kwargs.pop("webMod")  # these are the same thing as far as I'm aware
-        self._site_admin: bool = kwargs.pop("admin", False)
-        self._certified: bool = kwargs.pop("certified", False)  # if the user has a certified bot
+        self._supporter: bool = kwargs.get("supporter", False)
+        self._site_mod: bool = kwargs.get("mod") or kwargs.get("webMod")  # these are the same thing as far as I'm aware
+        self._site_admin: bool = kwargs.get("admin", False)
+        self._certified: bool = kwargs.get("certified", False)  # if the user has a certified bot
         if kwargs.get("state"):
             self._user: Optional[DiscordUser] = kwargs["state"].get_user(self.id)
         else:
@@ -286,33 +290,33 @@ class Bot(UserABC, _ReprMixin):
 
     def __init__(self, **kwargs):
         self.id: int = int(kwargs.pop("id"))
-        self.username: str = kwargs.pop("username")
-        self.discriminator: str = kwargs.pop("discriminator")
-        self.user_avatar: Optional[str] = kwargs.pop("avatar", None)
-        self.default_avatar: str = kwargs.pop("defAvatar")
+        self.username: str = kwargs.get("username")
+        self.discriminator: str = kwargs.get("discriminator")
+        self.user_avatar: Optional[str] = kwargs.get("avatar", None)
+        self.default_avatar: str = kwargs.get("defAvatar")
         self.avatar: str = self.user_avatar or self.default_avatar
 
-        self.prefix: str = kwargs.pop("prefix")
-        self.short_description: str = kwargs.pop("shortdesc")
-        self.long_description: Optional[str] = kwargs.pop("longdesc", None)  # NOTE: this can be empty for some reason
-        self.tags: List[str] = kwargs.pop("tags", [])
-        self.website: Optional[str] = kwargs.pop("website", None)
-        self.support: Optional[str] = kwargs.pop("support", None)
-        self.github: Optional[str] = kwargs.pop("github", None)
-        self.owners: List[int] = list(map(int, kwargs.pop("owners", [])))
-        self.featured_guilds: List[int] = list(map(int, kwargs.pop("guilds", [])))
-        self.invite: str = kwargs.pop("invite", None) or invite(str(self.id))
+        self.prefix: str = kwargs.get("prefix")
+        self.short_description: str = kwargs.get("shortdesc")
+        self.long_description: Optional[str] = kwargs.get("longdesc", None)  # NOTE: this can be empty for some reason
+        self.tags: List[str] = kwargs.get("tags", [])
+        self.website: Optional[str] = kwargs.get("website", None)
+        self.support: Optional[str] = kwargs.get("support", None)
+        self.github: Optional[str] = kwargs.get("github", None)
+        self.owners: List[int] = list(map(int, kwargs.get("owners", [])))
+        self.featured_guilds: List[int] = list(map(int, kwargs.get("guilds", [])))
+        self.invite: str = kwargs.get("invite", None) or invite(str(self.id))
         try:
             self.approved_at: datetime = (
-                datetime.strptime("%Y-%m-%dT%H:%M:%S.%fZ", kwargs.pop("date", "")) or datetime.min
+                datetime.strptime("%Y-%m-%dT%H:%M:%S.%fZ", kwargs.get("date", "")) or datetime.min
             )
         except ValueError:
             self.approved_at = datetime.min
-        self.certified: bool = kwargs.pop("certifiedBot", False)
-        self.vanity_uri: Optional[str] = kwargs.pop("vanity", None)
-        self.all_time_votes: int = kwargs.pop("points", 0)
-        self.monthly_votes: int = kwargs.pop("monthlyPoints", 0)
-        self.donations_guild: Optional[int] = kwargs.pop("donatebotguildid", None)
+        self.certified: bool = kwargs.get("certifiedBot", False)
+        self.vanity_uri: Optional[str] = kwargs.get("vanity", None)
+        self.all_time_votes: int = kwargs.get("points", 0)
+        self.monthly_votes: int = kwargs.get("monthlyPoints", 0)
+        self.donations_guild: Optional[int] = kwargs.get("donatebotguildid", None)
 
         if kwargs.get("state"):
             self._user: Optional[DiscordUser] = kwargs["state"].get_user(self.id)
